b4ef0caa1a65a89bf894c6ae2a109c24ebf9ecc3
nishizumi <yusuke059@gmail.com>
2020-03-26 00:06:18 +0900

    add ltspice

---
+++ python/basic/args/ex1.py
@@ -0,0 +1,6 @@
+import sys
+
+# argsは文字列型
+args = sys.argv
+
+print(args) # 引数の値が表示
---
+++ python/basic/args/ex2.py
@@ -0,0 +1,12 @@
+import sys
+
+# リスト型に文字列型の要素が格納されます。
+args = sys.argv
+
+print(args)
+
+"""
+$ex2.py Ex cali bur
+
+['ex2.py', 'Ex', 'cali', 'bur']
+"""
