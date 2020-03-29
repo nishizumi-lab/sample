caee569178639396a0243f01065375252342721c
nishizumi <yusuke059@gmail.com>
2020-03-29 22:16:37 +0900

    Update ex1.py

--- python/scikit/svm/ex1.py
+++ python/scikit/svm/ex1.py
@@ -17,18 +17,21 @@ train_y = train_data['x3'].values
 clf = svm.SVC(gamma=0.001, C=100.)
 clf.fit(train_X, train_y)
 
+# 学習結果を出力
+joblib.dump(clf, 'C:/github/sample/python/scikit/svm/data/train.learn')
+
+# 学習済ファイルのロード
+clf2 = joblib.load('C:/github/sample/python/scikit/svm/data/train.learn')
+
 # テスト用データの読み込み
 test_data = pd.read_csv("C:/github/sample/python/scikit/svm/data/test.csv", sep=",")
 
 # 学習結果の検証（テスト用データx1, x2を入力）
 test_X = test_data.loc[:, ['x1', 'x2']].values
-test_y = clf.predict(test_X)
+test_y = clf2.predict(test_X)
 
 # 検証結果の表示
 print("検証結果：", test_y)
-
-# 学習結果を出力
-joblib.dump(clf, 'C:/github/sample/python/scikit/svm/data/train.learn')
 
 """
 検証結果： [0 1 1 0]
