b29ad93cc58c719228020d12f563528d88f82aa6
nishi <yusuke059@gmail.com>
2020-04-05 11:01:16 +0900

    add

---
+++ python/scikit/dtc/ex1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from sklearn import tree
+
+LOAD_FILE_PATH = "C:/github/sample/python/scikit/dtc/sample_data/input.csv"
+SAVE_FILE_PATH = "C:/github/sample/python/scikit/dtc/sample_data/output.clf"
+
+# CSVファイルを取得
+data = pd.read_csv(LOAD_FILE_PATH, sep=",")
+
+# 説明変数(x1, x2に設定)
+variables = ['x1', 'x2']
+
+# 決定木の分類器を生成
+clf = tree.DecisionTreeClassifier()
+
+# 分類器にサンプルデータを入れて学習(目的変数はx）
+clf = clf.fit(data[variables], data['x3'])
+
+# 学習結果を出力
+with open(SAVE_FILE_PATH, 'w') as f:
+    f = tree.export_graphviz(clf, out_file=f)
+
+
+# 学習した分類器で予測
+pred = clf2.predict(data[['x1', 'x2']])
+
+# 予測結果を表示
+print(pred)
+
+# 識別率を表示
+print(sum(pred == data['x3']) / len(data[['x1', 'x2']]))
