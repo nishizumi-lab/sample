8a709c73d2639d7a07e66a1b051e83b4c68eb538
nishi <yusuke059@gmail.com>
2020-04-05 11:29:18 +0900

    add

--- python/scikit/dtc/ex1.py
+++ python/scikit/dtc/ex1.py
@@ -26,7 +26,7 @@ with open(SAVE_FILE_PATH, 'w') as f:
 clf2 = joblib.load(SAVE_FILE_PATH)
 
 # 学習済の分類器で予測
-pred2 = clf2.predict(data[['x1', 'x2']])
+pred = clf2.predict(data[['x1', 'x2']])
 
 # 予測結果を表示
 print(pred)
