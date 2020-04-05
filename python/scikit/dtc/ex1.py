07345bacb2a31408aef7e4e4283244b5c9fc80c2
nishi <yusuke059@gmail.com>
2020-04-05 11:19:11 +0900

    add

--- python/scikit/dtc/ex1.py
+++ python/scikit/dtc/ex1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from sklearn import tree
+import joblib
 
 LOAD_FILE_PATH = "C:/github/sample/python/scikit/dtc/sample_data/input.csv"
 SAVE_FILE_PATH = "C:/github/sample/python/scikit/dtc/sample_data/output.clf"
@@ -21,9 +22,11 @@ clf = clf.fit(data[variables], data['x3'])
 with open(SAVE_FILE_PATH, 'w') as f:
     f = tree.export_graphviz(clf, out_file=f)
 
+# 保存した学習済ファイルをロード
+clf2 = joblib.load(SAVE_FILE_PATH)
 
-# 学習した分類器で予測
-pred = clf2.predict(data[['x1', 'x2']])
+# 学習済の分類器で予測
+pred2 = clf2.predict(data[['x1', 'x2']])
 
 # 予測結果を表示
 print(pred)
