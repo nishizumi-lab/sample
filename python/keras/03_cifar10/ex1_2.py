cad73dc06fbc36814875031932b8baa8af8c5c74
Nishizumi <daiman003@yahoo.co.jp>
2020-05-05 19:31:36 +0900

    update

--- python/keras/03_cifar10/ex1_2.py
+++ python/keras/03_cifar10/ex1_2.py
@@ -14,16 +14,16 @@ def main():
     # 入力画像のパラメータ
     img_width = 32 # 入力画像の幅
     img_height = 32 # 入力画像の高さ
-    img_ch = 3 # 1ch画像（グレースケール）で学習
+    img_ch = 3 # 3ch画像（グレースケール）で学習
 
     # 入力データ数
     num_data = 1
 
     # データ格納用のディレクトリパス
-    SAVE_DATA_DIR_PATH = "/Users/panzer5/github/sample/python/keras/04_img/ex1_data/"
+    SAVE_DATA_DIR_PATH = "/Users/panzer5/github/sample/python/keras/03_cifar10/ex1_data/"
 
     # ラベル
-    labels =['やかん', '土鍋', 'マグカップ']
+    labels =['飛行機', '自動車', '鳥', '猫', '鹿', '犬', '蛙', '馬', '船', 'トラック']
 
     # 保存したモデル構造の読み込み
     model = model_from_json(open(SAVE_DATA_DIR_PATH + "model.json", 'r').read())
@@ -33,7 +33,7 @@ def main():
 
     # 画像の読み込み（32×32にリサイズ）
     # 正規化, 4次元配列に変換（モデルの入力が4次元なので合わせる）
-    img = load_img(SAVE_DATA_DIR_PATH + "test.jpg", target_size=(32, 32))
+    img = load_img(SAVE_DATA_DIR_PATH + "test2.png", target_size=(img_width, img_height))
     img = img_to_array(img) 
     img = img.astype('float32')/255.0
     img = np.array([img])
@@ -41,7 +41,7 @@ def main():
     # 分類機に入力データを与えて予測（出力：各クラスの予想確率）
     y_pred = model.predict(img)
 
-    # 最も確率の高い要素番号
+    # 最も確率の高い要素番号（=予想する数字）
     number_pred = np.argmax(y_pred) 
 
     # 予測結果の表示
@@ -49,11 +49,12 @@ def main():
     print("number_pred:", number_pred)  # 最も確率の高い要素番号
     print('label_pred：', labels[int(number_pred)]) # 予想ラベル（最も確率の高い要素）
 
-
     """
-    predict_y: [[1.2638741e-20 4.6908645e-21 1.0000000e+00]]
-    predict_number: 2
-    predict_label： マグカップ
+    y_pred: [[1.05510656e-10 9.90023911e-01 7.12201237e-16 6.99716381e-17
+    9.21270893e-22 1.03125948e-20 5.56179723e-13 2.53434248e-15
+    1.68777863e-08 9.97608900e-03]]
+    number_pred: 1
+    label_pred： 自動車
     """
 
 if __name__ == '__main__':
--- python/keras/04_img/ex1_2.py
+++ python/keras/04_img/ex1_2.py
@@ -14,16 +14,16 @@ def main():
     # 入力画像のパラメータ
     img_width = 32 # 入力画像の幅
     img_height = 32 # 入力画像の高さ
-    img_ch = 1 # 1ch画像（グレースケール）で学習
+    img_ch = 3 # 1ch画像（グレースケール）で学習
 
     # 入力データ数
     num_data = 1
 
     # データ格納用のディレクトリパス
-    SAVE_DATA_DIR_PATH = "/Users/panzer5/github/sample/python/keras/03_cifar10/ex1_data/"
+    SAVE_DATA_DIR_PATH = "/Users/panzer5/github/sample/python/keras/04_img/ex1_data/"
 
     # ラベル
-    labels =['飛行機', '自動車', '鳥', '猫', '鹿', '犬', '蛙', '馬', '船', 'トラック']
+    labels =['やかん', '土鍋', 'マグカップ']
 
     # 保存したモデル構造の読み込み
     model = model_from_json(open(SAVE_DATA_DIR_PATH + "model.json", 'r').read())
@@ -33,7 +33,7 @@ def main():
 
     # 画像の読み込み（32×32にリサイズ）
     # 正規化, 4次元配列に変換（モデルの入力が4次元なので合わせる）
-    img = load_img("input.jpg", target_size=(32, 32))
+    img = load_img(SAVE_DATA_DIR_PATH + "test.jpg", target_size=(32, 32))
     img = img_to_array(img) 
     img = img.astype('float32')/255.0
     img = np.array([img])
@@ -41,21 +41,19 @@ def main():
     # 分類機に入力データを与えて予測（出力：各クラスの予想確率）
     y_pred = model.predict(img)
 
-    # 最も確率の高い要素番号（=予想する数字）
+    # 最も確率の高い要素番号
     number_pred = np.argmax(y_pred) 
 
     # 予測結果の表示
-    print("predict_y:", y_pred)  # 出力値
-    print("predict_number:", number_pred)  # 最も確率の高い要素番号
-    print('predict_label：', labels[int(number_pred)]) # 予想ラベル（最も確率の高い要素）
-
+    print("y_pred:", y_pred)  # 出力値
+    print("number_pred:", number_pred)  # 最も確率の高い要素番号
+    print('label_pred：', labels[int(number_pred)]) # 予想ラベル（最も確率の高い要素）
 
 
     """
-    predict_y: [[2.0631208e-16 8.2029376e-11 1.0000000e+00 8.4496722e-13 4.3476162e-22
-    4.4720264e-21 3.8950523e-22 5.1041643e-18 1.4993143e-12 1.6509382e-13]]
+    predict_y: [[1.2638741e-20 4.6908645e-21 1.0000000e+00]]
     predict_number: 2
-    predict_classes_y: [2]
+    predict_label： マグカップ
     """
 
 if __name__ == '__main__':
