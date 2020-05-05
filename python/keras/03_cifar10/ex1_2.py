1bde908d1160c118fc53416ea0551cc10d479943
Nishizumi <daiman003@yahoo.co.jp>
2020-05-05 14:10:02 +0900

    Update ex1_2.py

--- python/keras/03_cifar10/ex1_2.py
+++ python/keras/03_cifar10/ex1_2.py
@@ -5,6 +5,7 @@ from keras.layers.core import Dense, Dropout
 from keras.optimizers import RMSprop
 from keras.datasets import mnist
 from keras.utils import np_utils, to_categorical
+from keras.preprocessing.image import load_img, img_to_array
 import matplotlib.pyplot as plt
 import cv2
 import os
@@ -19,7 +20,7 @@ def main():
     num_data = 1
 
     # データ格納用のディレクトリパス
-    SAVE_DATA_DIR_PATH = "/Users/panzer5/github/sample/python/keras/02_mnist/ex3_data/"
+    SAVE_DATA_DIR_PATH = "/Users/panzer5/github/sample/python/keras/03_cifar10/ex1_data/"
 
     # ラベル
     labels =['飛行機', '自動車', '鳥', '猫', '鹿', '犬', '蛙', '馬', '船', 'トラック']
@@ -30,37 +31,25 @@ def main():
     # 保存した学習済みの重みを読み込み
     model.load_weights(SAVE_DATA_DIR_PATH + "weight.hdf5")
 
-    # カメラ画像の整形
-    img = cv2.imread(SAVE_DATA_DIR_PATH + "test.png")
-
-    # グレースケールに変換
-    # 2値化, 白黒反転, ガウシアンフィルタで平滑化、リサイズ
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, th = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU) # 
-    th = cv2.bitwise_not(th) # 
-    th = cv2.GaussianBlur(th, (9,9), 0) # 
-    th = cv2.resize(th,(img_width, img_height), cv2.INTER_CUBIC) # 訓練データと同じサイズに整形
-
-    # float32に変換して正規化
-    th = th.astype('float32')
-    th = np.array(th)/255
-
-    # モデルの入力次元数に合わせてリサイズ
-    th = th.reshape(num_data, img_height, img_width, img_ch)
+    # 画像の読み込み（32×32にリサイズ）
+    # 正規化, 4次元配列に変換（モデルの入力が4次元なので合わせる）
+    img = load_img("input.jpg", target_size=(32, 32))
+    img = img_to_array(img) 
+    img = img.astype('float32')/255.0
+    img = np.array([img])
 
     # 分類機に入力データを与えて予測（出力：各クラスの予想確率）
-    predict_y = model.predict(th)
+    y_pred = model.predict(img)
 
     # 最も確率の高い要素番号（=予想する数字）
-    predict_number = np.argmax(predict_y) 
+    number_pred = np.argmax(y_pred) 
 
     # 予測結果の表示
-    print("predict_y:", predict_y)  # 出力値
-    print("predict_number:", predict_number)  # 予測した数字
+    print("predict_y:", y_pred)  # 出力値
+    print("predict_number:", number_pred)  # 最も確率の高い要素番号
+    print('predict_label：', labels[int(number_pred)]) # 予想ラベル（最も確率の高い要素）
 
-    # 分類機に入力データを与えて予測（出力：クラスラベル）
-    predict_classes_y = model.predict_classes(th)
-    print("predict_classes_y:", predict_classes_y)  # 予測した数字
+
 
     """
     predict_y: [[2.0631208e-16 8.2029376e-11 1.0000000e+00 8.4496722e-13 4.3476162e-22
