865170c17e46727c8e693de1651323fdc2d01edb
Nishizumi <daiman003@yahoo.co.jp>
2020-05-07 18:04:12 +0900

    update

--- python/keras/05_vgg16/ex2.py
+++ python/keras/05_vgg16/ex2.py
@@ -5,8 +5,8 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.layers import Input, Activation, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
+import pickle
 
 def build_model(nb_classes=5, width=150, height=150):
     # VGG16の読み込み（FC層は不要なので include_topはFalse）
@@ -28,14 +28,19 @@ def build_model(nb_classes=5, width=150, height=150):
 
 
 # 画像の生成
-def img_generator(classes, batch_size=16, train_path, validation_path):
+def img_generator(classes, 
+                train_path, 
+                validation_path, 
+                batch_size=16, 
+                img_width=32, 
+                img_height=32):
     # ディレクトリ内の画像を読み込んでトレーニングデータとバリデーションデータの作成
     train_gen = ImageDataGenerator(rescale=1.0 / 255, zoom_range=0.2, horizontal_flip=True)
 
     validation_gen = ImageDataGenerator(rescale=1.0 / 255)
 
     tg = train_gen.flow_from_directory(train_path,
-        target_size=(width, height),
+        target_size=(img_width, img_height),
         color_mode='rgb',
         classes=classes,
         class_mode='categorical',
@@ -44,7 +49,7 @@ def img_generator(classes, batch_size=16, train_path, validation_path):
 
     vg = validation_gen.flow_from_directory(
         validation_path,
-        target_size=(width, height),
+        target_size=(img_width, img_height),
         color_mode='rgb',
         classes=classes,
         class_mode='categorical',
@@ -58,14 +63,16 @@ def main():
     nb_epoch = 10
     nb_train_samples = 200
     nb_validation_samples = 50
+    batch_size = 16
+    img_width = 32
+    img_height = 32
     # 分類するクラス
     classes = ['yakan', 'donabe', 'mag']
     # トレーニング用とバリデーション用の画像格納先
-    train_path = 'train'
-    validation_path = 'validation'
+    SAVE_DATA_DIR_PATH = '/Users/panzer5/github/sample/python/keras/05_vgg16/ex2_data/'
 
     # モデル作成
-    model = vgg_model_maker()
+    model = build_model()
 
     # 最後のconv層の直前までの層をfreeze
     for layer in model.layers[:15]:
@@ -77,7 +84,12 @@ def main():
               metrics=['accuracy'])
 
     # 画像のジェネレータ生成
-    tg, vg = image_generator(classes, batch_size=16, train_path, validation_path)
+    tg, vg = img_generator(classes = classes,
+                            train_path = SAVE_DATA_DIR_PATH + "train/", 
+                            validation_path = SAVE_DATA_DIR_PATH + "validation/",
+                            batch_size = batch_size,
+                            img_width = img_width,
+                            img_height = img_height)
 
     # Fine-tuning
     history = model.fit_generator(tg,
@@ -86,8 +98,16 @@ def main():
         validation_data = vg,
         nb_val_samples = nb_validation_samples)
 
-    model.save_weights(os.path.join('./', 'train.h5'))
+ 
+    # モデル構造の保存
+    open(SAVE_DATA_DIR_PATH  + "model.json","w").write(model.to_json())  
 
+    # 学習済みの重みを保存
+    model.save_weights(SAVE_DATA_DIR_PATH + "weight.hdf5")
+
+    # 学習履歴を保存
+    with open(SAVE_DATA_DIR_PATH + "history.json", 'wb') as f:
+        pickle.dump(history.history, f)
 
 if __name__ == '__main__':
     main()
