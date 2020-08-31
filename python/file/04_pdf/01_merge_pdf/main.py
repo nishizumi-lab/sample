427f501ae407d648ff59198962b78b72ab57fdc8
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2020-08-31 23:47:50 +0900

    fix

---
+++ python/file/04_pdf/01_merge_pdf/main.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import PyPDF2
+
+# ロードするCSVファイルのパスと文字コード
+load_pdf_dir = "C:/github/sample/python/file/04_pdf/00_sample_data/"
+encode_type = "UTF-8"
+pdf_files = Path(load_pdf_dir)
+# ディレクトリを自動生成する先のパス
+merge_pdf_path = "C:/github/sample/python/file/04_pdf/01_merge_pdf/"
+
+pdf_files = pdf_files.glob("*.pdf")
+print(pdf_files)
+#pdf_files = sorted(pdf_dir)
+
+# １つのPDFファイルにまとめる
+pdf_writer = PyPDF2.PdfFileWriter()
+for pdf_file in pdf_files:
+    pdf_reader = PyPDF2.PdfFileReader(str(pdf_file))
+    for i in range(pdf_reader.getNumPages()):
+        pdf_writer.addPage(pdf_reader.getPage(i))
+
+# 保存ファイル名（先頭と末尾のファイル名で作成）
+merged_file = pdf_files[0].stem + "-" + pdf_files[-1].stem + ".pdf"
+
+# 保存
+with open(merged_file, "wb") as f:
+    pdf_writer.write(f)
+
+"""
+C:/github/sample/python/file/03_csv/01_list_to_dir/内に以下のディレクトリが作成されます。
+
+1-A組
+2-B組
+3-C組
+4-D組
+5-E組
+"""
