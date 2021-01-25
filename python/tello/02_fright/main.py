b350f374d76ee5a8f42d971761d37e8d92c717ed
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-01-25 21:49:25 +0900

    fix

---
+++ python/tello/02_fright/main.py
@@ -0,0 +1,48 @@
+import threading 
+import socket
+import sys
+import time
+
+host = ''
+port = 9000
+locaddr = (host,port) 
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+tello_address = ('192.168.10.1', 8889)
+
+sock.bind(locaddr)
+
+def recv():
+    count = 0
+    while True: 
+        try:
+            data, server = sock.recvfrom(1518)
+        except Exception:
+            print ('\nExit . . .\n')
+            break
+
+#recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+
+while True: 
+
+    try:
+        msg = input("");
+
+        if not msg:
+            break  
+
+        if 'end' in msg:
+            print ('...')
+            sock.close()  
+            break
+
+        # Send data
+        msg = msg.encode(encoding="utf-8") 
+        sent = sock.sendto(msg, tello_address)
+    except KeyboardInterrupt:
+        sock.close()  
+        break
