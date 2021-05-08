812438785a3d832dd8883b4504dbfeebbf1408e3
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-05-08 14:07:21 +0900

    add

--- python/pandas/count.py
+++ python/pandas/count.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-def main():
-    # CSVファイルの読み込み
-    df = pd.read_csv("artoria.csv", index_col=0)
-    # 計算
-    count = df['ATK'].count()
-    # 表示
-    print("count:",count)
+# CSVファイルの読み込み
+df = pd.read_csv("/Users/github/sample/python/pandas/artoria.csv", index_col=0)
 
+# 計算
+count = df['ATK'].count()
 
-if __name__ == '__main__':
-    main()
+# 表示
+print("count:",count)
