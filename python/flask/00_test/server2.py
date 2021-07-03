e70df0bd50b5f4996739d18be3b08a7cd04bf588
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-07-04 00:17:58 +0900

    Update server2.py

--- python/flask/00_test/server2.py
+++ python/flask/00_test/server2.py
@@ -3,11 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# ルーティング(クライアントからの要求内容に応じて、処理を切り換える)を記述
+# index(トップ)にアクセスしたときの処理
 @app.route('/')
 def index():
+    # クライアントに以下の文字列(HTML)を返す
     return '<html><span style="color:#ff0000;">Hello World</span></html>'
 
 
 if __name__ == '__main__':
+     # デバッグモード有効化
     app.debug = True
+    # アプリの起動設定(IPアドレスとポート番号)
     app.run(host='127.0.0.1', port=8080)
