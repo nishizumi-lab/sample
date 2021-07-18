29aaa8791fd4c4acc128fdebe173231a9ebf029e
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-07-18 10:26:26 +0900

    add

--- python/flask/04_todo/sample01/app.py
+++ python/flask/04_todo/sample01/app.py
@@ -4,33 +4,42 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# データベースの設定(sqliteファイルのパスを指定)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
-# モデル
+
+# Todoリストのモデルを定義(db.Modelクラスを継承する必要がある)
 class Todo(db.Model):
+    # テーブル名を設定(テーブル名はクラス名の複数形が一般的)
+    __tablename__ = 'todos'
+    # 作成するテーブルのカラムを定義
+    # ID
     id = db.Column(db.Integer, primary_key=True)
+    # コンテンツ
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
+    # 作成日
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 # ルートにアクセスされたらインデックページを開く
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    # POSTメソッドで要求されたら
     if request.method == 'POST':
+        # コンテンツを取得
         task_content = request.form['content']
+        # 新しいタスクを作成
         new_task = Todo(content=task_content)
 
         try:
+            # データベースに新しいタスクを登録しトップページにリダイレクト
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
             return "フォームの送信中に問題が発生しました"
-
+    # 要求がない場合は、タスクリストを日付順に並べて表示
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
@@ -38,20 +47,23 @@ def index():
 # 削除画面
 @app.route('/delete/<int:id>')
 def delete(id):
+    # 削除するタスクのIDを取得
     task_to_delete = Todo.query.get_or_404(id)
 
     try:
+        # 削除対象のタスクをDBから削除しトップページにリダイレクト
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was an problem deleting that task'
+        return '削除中に問題が発生しました'
 
 # 編集画面
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def update(id):
+    # 編集するタスクのIDを取得
     task_to_edit = Todo.query.get_or_404(id)
-
+    # POSTメソッドがきたら編集対象のIDのコンテンツを更新
     if request.method == 'POST':
         task_to_edit.content = request.form['content']
 
@@ -59,11 +71,13 @@ def update(id):
             db.session.commit()
             return redirect('/')
         except:
-            return "タスクの編集に問題が発生しました"
+            return "タスクの編集中に問題が発生しました"
     else:
         return render_template('edit.html', task=task_to_edit)
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # モデルからテーブルを作成(最初だけ)
+    #db.create_all()
+    # アプリを起動
+    app.run(host="127.0.0.1", port=8080)
