e01f6129191074c814844302f25c00d4a01ce0cc
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-07-21 23:12:15 +0900

    add

--- python/scraping/02_bs4/sample04.py
+++ python/scraping/02_bs4/sample04.py
@@ -1,19 +1,56 @@
-#-*- using:utf-8 -*-
-import urllib
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import urllib.error
+import urllib.request
+import os
+import time
 
-def check_url(url):
-    flag = True
+
+# ダウンロード(画像URL, 保存先パス)
+def download_img(url, dst_path):
     try:
-        f = urllib.request.urlopen(url)
-        print('OK:', url)
-        f.close()
-    except urllib.request.HTTPError:
-        print('Not found:', url)
-        flag = False
+        data = urllib.request.urlopen(url).read()
+        with open(dst_path, mode="wb") as f:
+            f.write(data)
+    except urllib.error.URLError as e:
+        print(e)
 
-    return flag
+# 画像の抽出先
+url = 'https://algorithm.joho.info/'
+
+# User-Agent
+ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
 
 
-url = "https://algorithm.joho.info"
+# リクエスト
+req = urllib.request.Request(url, headers={'User-Agent': ua})
 
-print(check_url(url)) # True
+# HTML取得
+html = urllib.request.urlopen(req)
+
+# HTMLパース
+soup = BeautifulSoup(html, "html.parser")
+
+# imgタグ
+img_tags = soup.find_all('img')
+
+# imgタグのURL格納用
+img_urls = []
+
+# imgタグのURLを抽出
+for img in img_tags:
+    img_urls.append(img.get('src'))
+
+# 保存先ディレクトリ
+dst_dir = './img'
+
+# ダウンロード間隔時間（サーバー負荷対策のため1sec以上は空ける）
+sleep_time = 1
+
+for img_url in img_urls:
+    filename = os.path.basename(img_url)
+    # 保存先パス=保存先ディレクトリ+ファイル名
+    dst_path = os.path.join(dst_dir, filename)
+    time.sleep(sleep_time)
+    print('DL:', img_url)
+    download_img(url, dst_path)
