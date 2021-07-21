1a5abe3ae0de8e333c4f685746afcaf5573fcbc7
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-07-21 22:52:15 +0900

    Update sample02.py

--- python/scraping/02_bs4/sample02.py
+++ python/scraping/02_bs4/sample02.py
@@ -1,19 +1,37 @@
-# -*- coding: utf-8
+# -*- coding: utf-8 -*-
 import urllib.request
+from bs4 import BeautifulSoup
 
-# url先のHTMLファイルを開く
-data = urllib.request.urlopen("https://algorithm.joho.info/" )
+# urlのHTMLを取得
+url = 'https://raw.githubusercontent.com/nishizumi-lab/sample/master/python/scraping/00_sample_data/sample01/index.html'
+html = urllib.request.urlopen(url)
 
-# HTMLの取得      
-html = data.read()
-html = html.decode('utf-8')
-    
-# 表示
-print(html)
-    
-# HTMLファイルを閉じる
-data.close()
+# HTMLパース
+soup = BeautifulSoup(html, 'html.parser')
+
+# 先頭のdivタグを取得
+div = soup.find('div')
+print('div=', div)
 
 """
-b'<!DOCTYPE html>\r\n<html lang="ja">\r\n<head>\r\n<meta charset="UTF-8"・・・・・・
+div= <div class="header">ヘッダーです</div>
+"""
+
+# すべてのdivタグを取得
+div_all = soup.find_all('div')
+print('div_all=', div_all)
+
+"""
+div_all= [
+<div class="header">ヘッダーです</div>, 
+<div class="main">
+<h1>H1の見出しです</h1>
+<p>文章1です</p>
+</div>, 
+<div class="footer">
+<p>フッターです</p>
+<a href="#">リンク1</a>
+<a href="#">リンク2</a>
+</div>
+]
 """
