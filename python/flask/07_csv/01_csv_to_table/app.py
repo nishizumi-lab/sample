96a53da85ca8dd86220b01034d47d07168d88b57
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-08-01 22:22:04 +0900

    add

--- python/flask/07_csv/01_csv_to_table/app.py
+++ python/flask/07_csv/01_csv_to_table/app.py
@@ -1,30 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-import numpy as np
-import os
-from datetime import datetime as dt
 import pandas as pd
 
-app = Flask(__name__, static_url_path="")
-
-# 処理した画像ファイルの保存先
-IMG_DIR = "/static/images/"
-BASE_DIR = os.path.dirname(__file__)
-IMG_PATH = BASE_DIR + IMG_DIR
-
-# 保存先のパスがなければ作成
-if not os.path.isdir(IMG_PATH):
-    os.mkdir(IMG_PATH)
-
-
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    date_fruit_list = pd.read_csv("https://raw.githubusercontent.com/nishizumi-lab/sample/master/python/flask/07_csv/00_sample_data/sample01.csv").values.tolist()
+    # CSVファイルをPandasでロード
+    df = pd.read_csv("https://raw.githubusercontent.com/nishizumi-lab/sample/master/python/flask/07_csv/00_sample_data/sample01.csv")
 
-    return render_template('index.html', title='食べた果物記録', date_fruit_list=date_fruit_list)
-## おまじない
-
+    # データフレームをリストに変換してテンプレートに渡す
+    return render_template('index.html', title='価格推移', data_lists=df.values.tolist())
 
 if __name__ == '__main__':
-
     app.run(host="127.0.0.1", port=8080)
