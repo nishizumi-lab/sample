893c11a49d62d1d52131ee19af7c70c7bd98267d
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-12-09 22:16:14 +0900

    Update sample01.py

--- python/tello/02_command_control/sample01.py
+++ python/tello/02_command_control/sample01.py
@@ -1,46 +1,50 @@
 import threading 
 import socket
-import sys
-import time
 
-host = ''
-port = 9000
-locaddr = (host,port) 
-
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-tello_address = ('192.168.10.1', 8889)
-
-sock.bind(locaddr)
-
+# Telloからのレスポンス受信
 def recv():
     count = 0
     while True: 
         try:
+            # クライアントからのメッセージの受信を受付開始(コネクションレス型)
             data, server = sock.recvfrom(1518)
         except Exception:
             print ('\nExit . . .\n')
             break
 
-#recvThread create
+host = ""
+port = 9000
+locaddr = (host,port) 
+
+# UDP通信ソケットの作成
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Tello側のローカルIPアドレス(デフォルト)、コマンドモード用の宛先ポート番号
+tello_address = ('192.168.10.1', 8889)
+
+# 自ホストで使用するIPアドレスとポート番号を設定
+sock.bind(locaddr)
+
+# 受信用スレッドの作成
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
 while True: 
 
     try:
-        msg = input("");
+        msg = input("")
 
+        # メッセージがなければ何もしない
         if not msg:
             break  
 
-        if 'end' in msg:
-            print ('...')
+        # 「q」でソケット通信終了
+        if 'q' in msg:
+            print ('QUIT...')
             sock.close()  
             break
 
-        # Send data
+        # データを送信
         msg = msg.encode(encoding="utf-8") 
         sent = sock.sendto(msg, tello_address)
 
@@ -48,8 +52,3 @@ while True:
         sock.close()  
         break
     
-"""
-command
-takeoff
-Land
-"""
