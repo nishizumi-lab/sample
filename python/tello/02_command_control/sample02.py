36ef4e32bdc5e8f6eae3be95771c5b43e0d2bc26
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-12-10 15:27:47 +0900

    Update sample02.py

--- python/tello/02_command_control/sample02.py
+++ python/tello/02_command_control/sample02.py
@@ -2,9 +2,9 @@
 import threading 
 import socket
 import time
-import tkinter
+import tkinter as tk
 
-class Application(tkinter.Frame):
+class Application(tk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
@@ -28,88 +28,100 @@ class Application(tkinter.Frame):
         askThread.setDaemon(True)
         askThread.start()
 
+
     # ウィジェットの作成
     def create_widgets(self):
+        self.battery_text = tk.StringVar()
+        self.time_text = tk.StringVar()
+        self.status_text = tk.StringVar()
+        self.battery_text.set("電池残量:")
+        self.time_text.set("飛行時間:")
+        self.status_text.set("ステータス:")
+
+
         # 電池残量
-        self.speed_text = tkinter.Label(text='電池:')
-        self.speed_text.place(x=0, y=0, width = 100, height = 20)
+        self.battery_label = tk.Label(text='電池残量:')
+        self.battery_label.place(x=0, y=0, width = 100, height = 20)
+        self.battery_label["textvariable"] = self.battery_text
 
         # 飛行時間
-        self.time_text = tkinter.Label(text='飛行時間:')
-        self.time_text.place(x=0, y=20, width = 100, height = 20)
+        self.time_label = tk.Label(text='飛行時間:')
+        self.time_label.place(x=0, y=20, width = 100, height = 20)
+        self.time_label["textvariable"] = self.time_text
 
         # 通信状況
-        self.status_text = tkinter.Label(text='通信状況:')
-        self.status_text.place(x=0, y=40, width = 100, height = 20)
+        self.status_label = tk.Label(text='ステータス:')
+        self.status_label.place(x=0, y=40, width = 100, height = 20)
+        self.status_label["textvariable"] = self.status_text
 
         # 離陸
-        self.takeoff = tkinter.Button()
+        self.takeoff = tk.Button()
         self.takeoff["text"] = "▲離陸"  # ボタンのテキスト
         self.takeoff["command"] = self.takeoff # ボタンが押されたらtakeoffメソッドを実行
         self.takeoff.place(x=0, y=60, width = 100, height = 20) # ボタンの位置
 
         # 着陸
-        self.land = tkinter.Button()
+        self.land = tk.Button()
         self.land["text"] = "▼着陸" 
         self.land["command"] = self.land
         self.land.place(x=0, y=80, width = 100, height = 20)
 
         # 上昇(20cm)
-        self.up = tkinter.Button()
+        self.up = tk.Button()
         self.up["text"] = "▲上昇"
         self.up["command"] = self.up 
         self.up.place(x=0, y=100, width = 100, height = 20)
 
         # 下降(20cm)
-        self.down = tkinter.Button()
+        self.down = tk.Button()
         self.down["text"] = "▼下降" 
         self.down["command"] = self.down 
         self.down.place(x=0, y=120, width = 100, height = 20)
 
         # 前に進む(20cm)
-        self.forward = tkinter.Button()
+        self.forward = tk.Button()
         self.forward["text"] = "▲前に進む" 
         self.forward["command"] = self.forward 
         self.forward.place(x=0, y=140, width = 100, height = 20)
 
         # 後に進む(20cm)
-        self.back = tkinter.Button()
+        self.back = tk.Button()
         self.back["text"] = "▼後に進む" 
         self.back["command"] = self.back 
         self.back.place(x=0, y=160, width = 100, height = 20)
 
         # 右に進む(20cm)
-        self.right = tkinter.Button()
+        self.right = tk.Button()
         self.right["text"] = "▶右に進む" 
         self.right["command"] = self.right 
         self.right.place(x=0, y=180, width = 100, height = 20)
 
         # 左に進む(20cm)
-        self.left = tkinter.Button()
+        self.left = tk.Button()
         self.left["text"] = "◀左に進む"
         self.left["command"] = self.left
         self.left.place(x=0, y=200, width = 100, height = 20)
 
         # 右回りに回転(90 deg)
-        self.cw = tkinter.Button()
+        self.cw = tk.Button()
         self.cw["text"] = "▶右回転(90度)" 
         self.cw["command"] = self.cw 
         self.cw.place(x=0, y=220, width = 100, height = 20)
 
         # 左回りに回転(45 deg)
-        self.ccw = tkinter.Button()
+        self.ccw = tk.Button()
         self.ccw["text"] = "◀左回転(90度)"
         self.ccw["command"] = self.ccw 
         self.ccw.place(x=0, y=240, width = 100, height = 20)
 
         # 高速モード(速度40cm/sec)
-        self.speed40 = tkinter.Button()
+        self.speed40 = tk.Button()
         self.speed40["text"] = "高速モード" 
         self.speed40["command"] = self.speed40 
         self.speed40.place(x=0, y=260, width = 100, height = 20)
 
         # 低速モード(速度20cm/sec)
-        self.speed20 = tkinter.Button()
+        self.speed20 = tk.Button()
         self.speed20["text"] = "低速モード" 
         self.speed20["command"] = self.speed20 
         self.speed20.place(x=0, y=280, width = 100, height = 20)
@@ -201,22 +213,25 @@ class Application(tkinter.Frame):
             sent = self.sock.sendto('speed 20'.encode(encoding="utf-8"), self.tello)
         except:
             pass
+
     # Telloからのレスポンス受信
     def recv(self):
         while True: 
             try:
                 data, server = self.sock.recvfrom(1518)
                 resp = data.decode(encoding="utf-8").strip()
-                if resp.isdecimal():    # 数字だけなら充電量
-                    self.batteryLabel.setText(resp + "%")
-                elif resp[-1:] == "s":  # 最後の文字がsなら飛行時間
-                    self.timeLabel.setText(resp)
-                elif resp == "OK":      # OKは黒
-                    self.label.setStyleSheet("color:black;")
-                    self.label.setText(resp)
-                else:                   # それ以外は赤
-                    self.label.setStyleSheet("color:red;")
-                    self.label.setText(resp)
+                # レスポンスが数字だけならバッテリー残量
+                if resp.isdecimal():    
+                    self.battery_text.set("電池残量:" + resp + "%")
+                # 最後の文字がsなら飛行時間
+                elif resp[-1:] == "s":
+                    self.time_text.set("飛行時間:" + resp + "秒")
+                # OKならステータス正常
+                elif resp == "OK":
+                    self.status_text.set("ステータス:正常")
+                # それ以外はステータス異常
+                else: 
+                    self.status_text.set("ステータス:異常")
             except:
                 pass
 
@@ -236,7 +251,7 @@ class Application(tkinter.Frame):
             time.sleep(0.5)
 
 if __name__ == '__main__':
-    root = tkinter.Tk()
+    root = tk.Tk()
     root.geometry("200x600")
     app = Application(master=root)
     app.mainloop()
