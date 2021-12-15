ea9a9d68111be3b08c58641a527b27e97e34e510
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2021-12-15 23:32:14 +0900

    Update sample01.py

--- python/tello/03_camera_control/sample01.py
+++ python/tello/03_camera_control/sample01.py
@@ -1,7 +1,17 @@
 import socket
 import threading
 import cv2
+import time
 import numpy as np
+
+# データ受け取り用の関数
+def udp_receiver():
+    while True:
+        try:
+            response, _ = socket.recvfrom(1024)
+        except Exception as e:
+            print(e)
+            break
 
 # Tello側のローカルIPアドレス(デフォルト)、宛先ポート番号(コマンドモード用)
 TELLO_IP = '192.168.10.1'
@@ -9,62 +19,62 @@ TELLO_PORT = 8889
 TELLO_ADDRESS = (TELLO_IP, TELLO_PORT)
 
 # Telloからの映像受信用のローカルIPアドレス、宛先ポート番号
-VS_UDP_IP = '0.0.0.0'
-VS_UDP_PORT = 11111
+TELLO_CAMERA_ADDRESS = 'udp://@0.0.0.0:11111'
 
-# データ受信用のオブジェクト準備
+# キャプチャ用のオブジェクト
+cap = None
+
+# データ受信用のオブジェクト備
 response = None
 
-# UDP通信ソケットの作成(アドレスファミリ：AF_INET（IPv4）、ソケットタイプ：SOCK_DGRAM（UDP）)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 通信用のソケットを作成
+# ※アドレスファミリ：AF_INET（IPv4）、ソケットタイプ：SOCK_DGRAM（UDP）
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # 自ホストで使用するIPアドレスとポート番号を設定
-sock.bind(('', TELLO_PORT))
-
-# データ受け取り用の関数
-def udp_receiver():
-    while True:
-        try:
-            # クライアントからのメッセージの受信を受付開始(コネクションレス型)
-            response, _ = sock.recvfrom(1024)
-        except Exception as e:
-            print(e)
-            break
+socket.bind(('', TELLO_PORT))
 
 # 受信用スレッドの作成
 thread = threading.Thread(target=udp_receiver, args=())
 thread.daemon = True
 thread.start()
 
+# コマンドモード
+socket.sendto('command'.encode('utf-8'), TELLO_ADDRESS)
+
+time.sleep(1)
+
 # カメラ映像のストリーミング開始
-sock.sendto('streamon'.encode('utf-8'), TELLO_ADDRESS)
+socket.sendto('streamon'.encode('utf-8'), TELLO_ADDRESS)
 
-tello_camera_address = 'udp://@' + VS_UDP_IP + ':' + str(VS_UDP_PORT)
+time.sleep(5)
 
-cap = cv2.VideoCapture(tello_camera_address)
+if cap is None:
+    cap = cv2.VideoCapture(TELLO_CAMERA_ADDRESS)
 
 if not cap.isOpened():
-    cap.open(tello_camera_address)
+    cap.open(TELLO_CAMERA_ADDRESS)
+
+time.sleep(10)
 
 while True:
-
     ret, frame = cap.read()
 
-    # frameが空やサイズが0のとき、スキップ
+    # 動画フレームが空ならスキップ
     if frame is None or frame.size == 0:
         continue
 
     # カメラ映像のサイズを半分にしてウィンドウに表示
     frame_height, frame_width = frame.shape[:2]
-    frame = cv2.resize(frame, (frame_height / 2, frame_width / 2))
-        
+    frame = cv2.resize(frame, int(frame_width/2), (int(frame_height/2)))
+    
     cv2.imshow('Tello Camera View', frame)
 
     # qキーで終了
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        # カメラ映像のストリーミング停止
-        sock.sendto('streamoff'.encode('utf-8'), TELLO_ADDRESS)
         break
-
 cap.release()
 cv2.destroyAllWindows()
+
+# ビデオストリーミング停止
+socket.sendto('streamoff'.encode('utf-8'), TELLO_ADDRESS)
