e7ed1c535df9f06bc261f25ced40e32504430459
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2022-04-13 19:48:07 +0900

    Update sample02.ino

--- m5stack/05_bmp280/sample02/sample02.ino
+++ m5stack/05_bmp280/sample02/sample02.ino
@@ -19,13 +19,13 @@ char *password = "XXXXXX";
 // GoogleスプレッドシートのデプロイされたURLを設定
 const char* published_url = "https://script.google.com/macros/s/XXXXXX/exec";
 
-void setup_serial(){
+void setup_serial() {
   Serial.begin(115200);
   while (!Serial) continue;
 }
 
-void setup_wifi(){
-  
+void setup_wifi() {
+
   Serial.println("Connecting to ");
   Serial.print(ssid);
 
@@ -35,9 +35,9 @@ void setup_wifi(){
 
   // WiFi開始
   WiFi.begin(ssid, password);
- 
+
   // Wi-Fi接続待ち
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     M5.Lcd.print(".");
   }
@@ -52,26 +52,26 @@ void setup_wifi(){
   // M5StackのIPアドレスを表示
   M5.Lcd.print("IP address: ");
   M5.Lcd.println(WiFi.localIP());
-  
+
   M5.Lcd.print("Please push button A \n");
 }
 
-void setup(){
+void setup() {
 
-    M5.begin();
-    Wire.begin();
+  M5.begin();
+  Wire.begin();
 
-    M5.Lcd.setBrightness(10);
+  M5.Lcd.setBrightness(10);
 
-    Serial.println(F("ENV Unit(DHT12 and BMP280) test..."));
+  Serial.println(F("ENV Unit(DHT12 and BMP280) test..."));
 
-    while (!bme.begin(0x76)){  
-      Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-      M5.Lcd.println("Could not find a valid BMP280 sensor, check wiring!");
-    }
-    // LCD初期化
-    M5.Lcd.clear(BLACK);
-    M5.Lcd.println("ENV Unit test...");
+  while (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    M5.Lcd.println("Could not find a valid BMP280 sensor, check wiring!");
+  }
+  // LCD初期化
+  M5.Lcd.clear(BLACK);
+  M5.Lcd.println("ENV Unit test...");
 
   setup_serial();
 
@@ -80,67 +80,67 @@ void setup(){
 
 }
 
-void loop(){
+void loop() {
 
   StaticJsonDocument<500> doc;
   char pubMessage[256];
 
   M5.update();
-  
-    // 温度の取得
-    float tmp = dht12.readTemperature();
 
-    // 湿度の取得
-    float hum = dht12.readHumidity();
+  // 温度の取得
+  float tmp = dht12.readTemperature();
 
-    // 気圧の取得[hPa = Pa * 0.01]
-    float pressure = bme.readPressure() * 0.01;
+  // 湿度の取得
+  float hum = dht12.readHumidity();
 
-    // 温度、湿度、気圧をシリアル通信で送信
-    Serial.printf("Temperatura: %2.2f*C  Humedad: %0.2f%%  Pressure: %0.2fPa\r\n", tmp, hum, pressure);
+  // 気圧の取得[hPa = Pa * 0.01]
+  float pressure = bme.readPressure() * 0.01;
 
-    // LCDに温度、湿度、気圧を表示
-    M5.Lcd.setCursor(0, 0); // カーソル
-    M5.Lcd.setTextColor(WHITE, BLACK);  // 色
-    M5.Lcd.setTextSize(4);  // 文字サイズ
-    M5.Lcd.printf("Temp:%2.1f \nHumi:%2.0f%% \nPres:%2.0fhPa \n", tmp, hum, pressure);
+  // 温度、湿度、気圧をシリアル通信で送信
+  Serial.printf("Temperatura: %2.2f*C  Humedad: %0.2f%%  Pressure: %0.2fPa\r\n", tmp, hum, pressure);
 
-    // JSONメッセージの作成
-    JsonArray tmpValues = doc.createNestedArray("temp");
-    tmpValues.add(String(tmp));
-    
-    JsonArray humValues = doc.createNestedArray("humid");
-    humValues.add(String(hum));
+  // LCDに温度、湿度、気圧を表示
+  M5.Lcd.setCursor(0, 0); // カーソル
+  M5.Lcd.setTextColor(WHITE, BLACK);  // 色
+  M5.Lcd.setTextSize(4);  // 文字サイズ
+  M5.Lcd.printf("Temp:%2.1f \nHumi:%2.0f%% \nPres:%2.0fhPa \n", tmp, hum, pressure);
 
-    JsonArray pressValues = doc.createNestedArray("pressure");
-    pressValues.add(String(pressure));
+  // JSONメッセージの作成
+  JsonArray tmpValues = doc.createNestedArray("temp");
+  tmpValues.add(String(tmp));
 
-    serializeJson(doc, pubMessage);
+  JsonArray humValues = doc.createNestedArray("humid");
+  humValues.add(String(hum));
 
-    // HTTP通信開始
-    HTTPClient http;
+  JsonArray pressValues = doc.createNestedArray("pressure");
+  pressValues.add(String(pressure));
 
-    Serial.print(" HTTP通信開始　\n");
-    http.begin(published_url);
-   
-    Serial.print(" HTTP通信POST　\n");
-    int httpCode = http.POST(pubMessage);
-   
-    if(httpCode > 0){
-      M5.Lcd.printf(" HTTP Response:%d\n", httpCode);
-   
-      if(httpCode == HTTP_CODE_OK){
-        M5.Lcd.println(" HTTP Success!!");
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-    }else{
-      M5.Lcd.println(" FAILED");
-      Serial.printf("　HTTP　failed,error: %s\n", http.errorToString(httpCode).c_str());
+  serializeJson(doc, pubMessage);
+
+  // HTTP通信開始
+  HTTPClient http;
+
+  Serial.print(" HTTP通信開始　\n");
+  http.begin(published_url);
+
+  Serial.print(" HTTP通信POST　\n");
+  int httpCode = http.POST(pubMessage);
+
+  if (httpCode > 0) {
+    M5.Lcd.printf(" HTTP Response:%d\n", httpCode);
+
+    if (httpCode == HTTP_CODE_OK) {
+      M5.Lcd.println(" HTTP Success!!");
+      String payload = http.getString();
+      Serial.println(payload);
     }
-   
-    http.end();
+  } else {
+    M5.Lcd.println(" FAILED");
+    Serial.printf("　HTTP　failed,error: %s\n", http.errorToString(httpCode).c_str());
+  }
 
-    delay(3600000);
-   
+  http.end();
+
+  delay(3600000);
+
 }
