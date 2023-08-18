6ff0a39a2bf74772cd9df6f39266a1c40dd0aa2d
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2023-08-19 01:55:55 +0900

    add

---
+++ wordpress/01_plugin/sample02/test-plugin2.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: プラグイン名
+Plugin URI: プラグインの説明と更新を示すページのURI
+Description: プラグインの説明
+Version: プラグインのバージョン番号
+Author: プラグインの作者名
+Author URI: プラグインの作者のURI
+License: ライセンス名(GPL2など)
+*/
+
