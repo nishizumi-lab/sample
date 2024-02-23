6f649ac6aa3d318ad16c80107b5a8676b0190c1d
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-02-23 22:02:45 +0900

    fix

---
+++ python/opencv/histgram/sample02.py
@@ -0,0 +1,40 @@
+#-*- coding:utf-8 -*-
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# 入力画像を読み込み
+img = cv2.imread("/Users/github/sample/python/opencv/histgram/input.jpg")
+
+img_b, img_g, img_r = img[:,:,0], img[:,:,1], img[:,:,2]
+
+# 方法1(NumPyでヒストグラムの算出)
+# hist_r, bins = np.histogram(r.ravel(),256,[0,256])
+# hist_g, bins = np.histogram(g.ravel(),256,[0,256])
+# hist_b, bins = np.histogram(b.ravel(),256,[0,256])
+
+# 方法2(OpenCVでヒストグラムの算出)
+hist_r = cv2.calcHist([img_r],[0],None,[256],[0,256])
+hist_g = cv2.calcHist([img_g],[0],None,[256],[0,256])
+hist_b = cv2.calcHist([img_b],[0],None,[256],[0,256])
+
+print('hist_r=')
+print(hist_r)
+
+print('hist_g=')
+print(hist_g)
+
+print('hist_b=')
+print(hist_b)
+
+# グラフの作成
+plt.xlim(0, 255)
+plt.plot(hist_r, "-r", label="Red")
+plt.plot(hist_g, "-g", label="Green")
+plt.plot(hist_b, "-b", label="Blue")
+plt.xlabel("Pixel value", fontsize=20)
+plt.ylabel("Number of pixels", fontsize=20)
+plt.legend()
+plt.grid()
+plt.show()
