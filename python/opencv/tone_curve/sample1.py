3f1f5c7399032fd17d729c1cd0b42acfbc4a6fc8
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-02-25 12:03:34 +0900

    fix

--- python/opencv/tone_curve/sample1.py
+++ python/opencv/tone_curve/sample1.py
@@ -1,12 +1,12 @@
 #-*- coding:utf-8 -*-
-import cv2
+import cv2 as cv
 import numpy as np
 
 # 入力画像を読み込み
-img = cv2.imread("C:/github/sample/python/opencv/tone_curve/input.jpg")
+img = cv.imread("/Users/github/sample/python/opencv/tone_curve/input.jpg")
 
 # グレースケール変換
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
 # 線形濃度変換
 a, k = 0.7, 20
@@ -29,4 +29,4 @@ gray[gray < 0] = 0
 gray[gray > 255] = 255
 
 # 結果の出力
-cv2.imwrite("C:/github/sample/python/opencv/tone_curve/output1.jpg", gray)
+cv.imwrite("C:/github/sample/python/opencv/tone_curve/output1.jpg", gray)
