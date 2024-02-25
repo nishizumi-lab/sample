5f6fb7e9b5fa8d3da3c2c3c046aab6e0b02095ad
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-02-25 14:45:50 +0900

    fix

--- python/opencv/tone_curve/gamma.py
+++ python/opencv/tone_curve/gamma.py
@@ -1,12 +1,12 @@
 #-*- coding:utf-8 -*-
-import cv2
+import cv2 as cv
 import numpy as np
 
 # 入力画像を読み込み
-img = cv2.imread("C:/github/sample/python/opencv/tone_curve/input.jpg")
+img = cv.imread("/Users/github/sample/python/opencv/tone_curve/input.jpg")
 
 # グレースケール変換
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
 # 線形濃度変換
 gamma = 0.5
@@ -18,4 +18,4 @@ imax = gray.max()
 gray = imax * (gray / imax)**(1/gamma)
 
 # 結果の出力
-cv2.imwrite("C:/github/sample/python/opencv/tone_curve/output2.jpg", gray)
+cv.imwrite("/Users/github/sample/python/opencv/tone_curve/output2.jpg", gray)
--- python/opencv/tone_curve/gamma_lut.py
+++ python/opencv/tone_curve/gamma_lut.py
@@ -1,12 +1,12 @@
 #-*- coding:utf-8 -*-
-import cv2
+import cv2 as cv
 import numpy as np
 
 # 入力画像を読み込み
-img = cv2.imread("C:/github/sample/python/opencv/tone_curve/input.jpg")
+img = cv.imread("/Users/github/sample/python/opencv/tone_curve/input.jpg")
 
 # グレースケール変換
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
 # 線形濃度変換
 gamma = 0.5
@@ -21,7 +21,7 @@ for i in range(256):
 	lookup_table[i][0] = imax * pow(float(i) / imax, 1.0 / gamma)
 
 # ルックアップテーブルで計算
-gray_gamma = cv2.LUT(gray, lookup_table)
+gray_gamma = cv.LUT(gray, lookup_table)
 
 # 結果の出力
-cv2.imwrite("C:/github/sample/python/opencv/tone_curve/output3.jpg", gray_gamma)
+cv.imwrite("C:/github/sample/python/opencv/tone_curve/output3.jpg", gray_gamma)
