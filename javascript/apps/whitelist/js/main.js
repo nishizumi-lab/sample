1bffe5879f68ccc3d00364c9764f85d784c2fc7f
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-03-06 16:15:54 +0900

    Update main.js

--- javascript/apps/whitelist/js/main.js
+++ javascript/apps/whitelist/js/main.js
@@ -1,26 +1,36 @@
 function main(){
     var inputData = document.getElementById('inputArea').value;
     var whiteListData = document.getElementById('whiteListArea').value;
-    var inputArray = inputData.split(~inputData.indexOf("\r\n")?"\r\n":"\n");
-    var whiteListArray = whiteListData.split(~whiteListData.indexOf("\r\n")?"\r\n":"\n");
+    var inputArray = inputData.split("\n");
+    var whiteListArray = whiteListData.split("\n");
 
     console.log("Input Data");
     console.log(inputArray);
     
     
     for(const whiteStr of whiteListArray){
-        const indexOfPigeon = inputArray.findIndex(animal => animal.includes(whiteStr))
-        inputArray.splice(indexOfPigeon, 1);
+        var i = 0;
+        for(const inputStr of inputArray){
+            if(inputStr.indexOf(whiteStr) > -1){
+                inputArray[i] = "DELETE";
+            }
+            i = i + 1;
+
+        }
     }
     
     console.log("Output Data");
     console.log(inputArray);
 
     // 重複要素を削除
-    var outputArray = new Set(inputArray);
+    var outputArray = Array.from(new Set(inputArray));
     
+    console.log("outputArray");
+    console.log(outputArray);
+
     var output = document.getElementById('outputArea');
     output.innerHTML = "";
+    
     var i = 0;
     
     while(i < inputArray.length){
