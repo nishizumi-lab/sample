54f2270c153681c54a4a8c785121ec1d579a8b59
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-03-16 10:36:02 +0900

    fix

--- python/jma/01_get_weather_forecast/get_weather_forecast.py
+++ python/jma/01_get_weather_forecast/get_weather_forecast.py
@@ -3,7 +3,7 @@ import requests
 
 # 気象庁から天気予報情報(JSONデータ)を取得
 # ファイルパスの「280000」はエリアコード。取得したい地域に応じて適宜変更します。
-JSON_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/280000.json"
+JSON_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json"
 jma_json = requests.get(JSON_URL).json()
 
 print(jma_json)
