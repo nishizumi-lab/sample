366cb3248b7cd6985c4b7b7e96c6173faf72fb94
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-03-24 23:44:45 +0900

    fix

--- python/flask/00_test/server2.py
+++ python/flask/00_test/server2.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 
+# Webアプリを作成
 app = Flask(__name__)
 
-# ルーティング(クライアントからの要求内容に応じて、処理を切り換える)を記述
-# index(トップ)にアクセスしたときの処理
+# 1つ目のルーティングを作成
+# パスに「'/'」を記述した場合、「http://127.0.0.1:8080/」(トップページ)にアクセスしたときの処理を関数に記述する
 @app.route('/')
 def index():
     # クライアントに以下の文字列(HTML)を返す
-    return '<html><br><br><span style="color:#ff0000;">■■■■■■■■■■Hello World!■■■■■■■■■■■</span></html>'
+    return '<html><br><br>トップページです</html>'
 
+# 2つ目のルーティングを作成()
+# パスに「'/login'」を記述した場合、「http://127.0.0.1:8080/login」にアクセスしたときの処理を関数に記述する
+@app.route('/login')
+def login():
+    # クライアントに以下の文字列(HTML)を返す
+    return '<html><br><br>ログインページページです</html>'
 
 if __name__ == '__main__':
-     # デバッグモード有効化
-    app.debug = True
-    # アプリの起動設定(IPアドレスとポート番号)
-    app.run(host='127.0.0.1', port=8080)
+    # Flaskのインスタンスを実行
+    # hostはホスト名。デフォルトはlocalhost(127.0.0.1)=自分のPC。
+    # portはポート番号。デフォルトは5000。
+    # debugはデバッグモードの有効。デフォルトはFalse。
+    app.run(host='127.0.0.1', port=8080, debug = True)
+    
+    
