463f53bb79558a54ae7b32b2f6069163fd5009fe
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-03-25 12:56:53 +0900

    fix

--- python/flask/02_template/sample02/server.py
+++ python/flask/02_template/sample02/server.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, url_for, request, redirect
-import time, datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name1 = "ユーザー1"
-    message1 = "メッセージ"
-
-    # 現在の時刻を取得
-    today = datetime.datetime.fromtimestamp(time.time())
-    message1 = today.strftime('%Y/%m/%d %H:%M:%S')
-
-    # index.htmlの変数展開(message1とname1の値がHTMLのmessage, nameに渡される)
+    message_hello = "こんにちは"
     return render_template('index.html',
-                           message=message1, name=name1)
+                           message=message_hello)
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='127.0.0.1', port=8080, debug = True)
