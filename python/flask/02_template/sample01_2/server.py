8cdead47ecb75d61dec01f6b3374a234f1820716
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-03-26 21:41:11 +0900

    fix

---
+++ python/flask/02_template/sample01_2/server.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    message_hello = "トップページです"
+    return render_template('index.html',
+                           message=message_hello)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug = True)
