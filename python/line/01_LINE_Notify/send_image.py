3b377dda93a4f876843f50dddbecab21053607ca
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-03-28 20:36:43 +0900

    fix

---
+++ python/line/01_LINE_Notify/send_image.py
@@ -0,0 +1,16 @@
+import requests
+
+LINE_NOTIFY_TOKEN= '取得したLINEのトークン'
+message = 'ご安全に！'
+filepath = '/Users/github/sample/python/line/01_LINE_Notify/mujiko.png'
+
+url = "https://notify-api.line.me/api/notify"
+headers = {"Authorization" : "Bearer "+ LINE_NOTIFY_TOKEN}
+payload = {"message" :  message}
+
+# 画像ファイル()
+files = {'imageFile': open(filepath, "rb")}
+
+r = requests.post(url ,headers = headers ,params=payload, files=files)
+
+print(r)
