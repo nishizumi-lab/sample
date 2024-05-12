3abdc2085dd770468af1a9eac294840f0692d11a
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2024-05-12 15:11:17 +0900

    Update ex1.py

--- python/pandas/basic/ex1.py
+++ python/pandas/basic/ex1.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-# リスト作成
-data = [158, 157, 157]
 
-# インデックスラベル
+# ①リストからSeriesを作成
+list_data = [158, 157, 157]
 index_data = ['miho','saori','yukari']
 
-# リストをSeriesに変換
-series = pd.Series(data, index=index_data)
+series = pd.Series(list_data, index=index_data)
 print(series)
 
 """
@@ -21,3 +19,20 @@ dtype: int64
 
 # ラベルを指定して値を取り出し
 print(series["miho"]) # 158
+
+# ②辞書からSeriesを作成
+dict_data = {'miho': 158, 'saori': 157, 'yukari': 157}
+
+
+
+# リストからSeriesに変換
+series2 = pd.Series(dict_data)
+print(series2)
+
+"""
+miho      158
+saori     157
+yukari    157
+
+dtype: int64
+"""
