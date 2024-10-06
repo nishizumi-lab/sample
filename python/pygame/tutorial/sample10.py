9dd82b35ce08acf145eae074330f1a770b8d7535
Nishizumi <34793405+nishizumi-lab@users.noreply.github.com>
2024-10-06 18:41:06 +0900

    Update sample10.py

--- python/pygame/tutorial/sample10.py
+++ python/pygame/tutorial/sample10.py
@@ -39,19 +39,19 @@ def main():
         # 画面の背景色（RGBA）     
         screen.fill((0, 0, 0, 0))
 
-        # キャラクターが画面の範囲外に出ないための処理
-        # 中心のx座標が0より小さければ0にする 
-        if player_rect.centerx < 0:
-            player_rect.centerx = 0
-        # 中心のx座標が画面の幅より大きければ画面左端までにする
-        if player_rect.centerx > SCREEN_WIDTH:
-            player_rect.centerx = SCREEN_WIDTH
-        # 中心のy座標が0より小さければ0にする 
-        if player_rect.centery < 0:
-            player_rect.centery = 0
-        # 中心のx座標が画面の高さより大きければ画面下端までにする
-        if player_rect.centery > SCREEN_HEIGHT:
-            player_rect.centery = SCREEN_HEIGHT 
+        # プレイヤーが画面の範囲外に出ないための処理
+        # 左端のx座標が0より小さければ0にする 
+        if player_rect.left < 0:
+            player_rect.left = 0
+        # 右端のx座標が画面の幅より大きければ画面左端までにする
+        if player_rect.right > SCREEN_WIDTH:
+            player_rect.right = SCREEN_WIDTH
+        # 上端のy座標が0より小さければ0にする 
+        if player_rect.top < 0:
+            player_rect.top = 0
+        # 下端のy座標が画面の高さより大きければ画面下端までにする
+        if player_rect.bottom > SCREEN_HEIGHT:
+            player_rect.bottom = SCREEN_HEIGHT 
 
         # 背景画像の描画
         screen.blit(bg, bg_rect)
@@ -72,13 +72,13 @@ def main():
                     sys.exit()
                 # 矢印キーなら円の中心座標を矢印の方向に移動
                 if event.key == K_LEFT:
-                    player_rect.centerx -= 5
+                    player_rect.centerx -= 15
                 if event.key == K_RIGHT:
-                    player_rect.centerx += 5
+                    player_rect.centerx += 15
                 if event.key == K_UP:
-                    player_rect.centery -= 5
+                    player_rect.centery -= 15
                 if event.key == K_DOWN:
-                    player_rect.centery += 5
+                    player_rect.centery += 15
 
 if __name__ == "__main__":
     main()
