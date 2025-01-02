958450e1b697cff262259a5f50654094e57f057a
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2025-01-02 23:19:32 +0900

    fix

--- python/opencv/grayscale/sample02.py
+++ python/opencv/grayscale/sample02.py
@@ -3,23 +3,26 @@ import cv2
 import numpy as np
 
 def rgb_to_gray(src):
-     # チャンネル分解
-     b, g, r = src[:,:,0], src[:,:,1], src[:,:,2]
+     # 画像の各チャンネルを分割
+     # src(RGBカラー画像)は3次元のNumPy配列(3チャンネル)
+     # blue, green, redは2次元のNumPy配列(1チャンネル)
+     blue, green, red = src[:,:,0], src[:,:,1], src[:,:,2]
 
      # R, G, Bの値からGrayの値に変換（float型 → 符号なし8bit整数型に変換）
-     return np.array(0.3 * r + 0.59 * g + 0.11 * b, dtype='uint8')
+     # # ここでは、一般的なグレースケール変換の重み付けを使用
+     return np.array(0.3 * red + 0.59 * green + 0.11 * blue, dtype='uint8')
 
 # 入力画像の読み込み
-img = cv2.imread("C:\prog\python\input.png")
+img = cv2.imread("C:/github/sample/python/opencv/grayscale/input.png")
 
 # グレースケール変換
 gray = rgb_to_gray(img)
 
 # グレースケール画像の書き込み
-cv2.imwrite("C:\prog\python\gray.png", gray)
+cv2.imwrite("C:/github/sample/python/opencv/grayscale/gray2.png", gray)
 
 
-# BGR, B, G, R, Grayの2次元配列を確認
+# BGR, B, G, R, Grayの中身を確認
 print("BGR=", img)
 print("-------------")
 
@@ -31,4 +34,5 @@ print("-------------")
 
 print("Red=", img[:,:,2])
 print("-------------")
+
 print("gray=", gray)
