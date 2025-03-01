12c0c74b66a59b5fcabfd1c2731f219f9c1e3cea
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2025-03-01 23:45:34 +0900

    fix

--- python/opencv/filter2d/non-local-means/sample01.py
+++ python/opencv/filter2d/non-local-means/sample01.py
@@ -6,7 +6,7 @@ import cv2 as cv
 img = cv.imread('C:/github/sample/python/opencv/filter2d/bilateral/input.jpg')
 
 # 非局所的平均フィルタを適用
-filtered = cv.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+filtered = cv.fastNlMeansDenoisingColored(img, None, 5, 10, 7, 21)
 
 # 結果を保存
 cv.imwrite('C:/github/sample/python/opencv/filter2d/bilateral/output.jpg', filtered)
