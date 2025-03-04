3abd7db1783f89681c21ff7ca7d3c3dc533d5f13
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2025-03-04 23:30:37 +0900

    fix

--- python/opencv/fft/lowpass_filter2.py
+++ python/opencv/fft/lowpass_filter2.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2
 
-def lowpass_filter(src, param = 0.5):
+def lowpass_filter(src, sigma=0.5):
     # 高速フーリエ変換(2次元)
     src = np.fft.fft2(src)
 
@@ -12,17 +12,18 @@ def lowpass_filter(src, param = 0.5):
     # 画像の中心座標
     cy, cx =  int(height/2), int(width/2)
 
-    # フィルタのサイズ(矩形の高さと幅)
-    rh, rw = int(param*cy), int(param*cx)
-
     # 第1象限と第3象限、第1象限と第4象限を入れ替え
     fsrc =  np.fft.fftshift(src)
 
     # 入力画像と同じサイズで値0の配列を生成
     fdst = np.zeros(src.shape, dtype=complex)
 
-    # 中心部分の値だけ代入（中心部分以外は0のまま）
-    fdst[cy-rh:cy+rh, cx-rw:cx+rw] = fsrc[cy-rh:cy+rh, cx-rw:cx+rw]
+    # ガウス分布のマスクを生成
+    y, x = np.ogrid[:height, :width]
+    mask = np.exp(-((x - cx)**2 + (y - cy)**2) / (2 * (sigma * min(height, width) / 2)**2))
+
+    # ガウス分布でフィルタリング(高周波成分ほど値を小さくする)
+    fdst = fsrc * mask
 
     # 第1象限と第3象限、第1象限と第4象限を入れ替え(元に戻す)
     fdst =  np.fft.fftshift(fdst)
@@ -35,8 +36,8 @@ def lowpass_filter(src, param = 0.5):
 
 
 def main():
-    # フィルタのサイズ(倍率)、小さいほどフィルタの影響が強くなる
-    param = 0.3
+    # ガウス分布のパラメータ（小さいほどフィルタの影響が強くなる)
+    sigma = 0.3
 
     # 入力画像を読み込み
     img = cv2.imread("C:/github/sample/python/opencv/fft/sample.jpg")
@@ -45,15 +46,15 @@ def main():
     img_blue, img_green, img_red = cv2.split(img)
 
     # ローパスフィルタ処理
-    himg_blue = lowpass_filter(img_blue, param)
-    himg_green = lowpass_filter(img_green, param)
-    himg_red = lowpass_filter(img_red, param)
+    himg_blue = lowpass_filter(img_blue, sigma)
+    himg_green = lowpass_filter(img_green, sigma)
+    himg_red = lowpass_filter(img_red, sigma)
 
     # RGB画像に戻す
     himg = cv2.merge((himg_blue, himg_green, himg_red))
 
     # 処理結果を出力
-    cv2.imwrite("C:/github/sample/python/opencv/fft/lowpass_filter.jpg", himg)
+    cv2.imwrite("C:/github/sample/python/opencv/fft/lowpass_filter2.jpg", himg)
 
 if __name__ == "__main__":
     main()
