7c4baae67dcf8ae705c26dd808c1257c28021064
Nishizimi LAB <34793405+nishizumi-lab@users.noreply.github.com>
2025-03-09 21:14:51 +0900

    fix

--- python/opencv/filter2d/integral-image/sample04.py
+++ python/opencv/filter2d/integral-image/sample04.py
@@ -10,9 +10,9 @@ img = cv.imread("C:/github/sample/python/opencv/filter2d/integral-image/input.jp
 start_time = time.time()
 
 # 各チャネルに対してメディアンフィルタを適用
-channels = cv.split(img)
-filtered_channels = [cv.medianBlur(c, ksize=5) for c in channels]
-dst = cv.merge(filtered_channels)
+# チャンネル分割して、各チャンネルに積分画像を用いた平均値フィルタを適用し、結合
+blue, green, red = cv.split(img)
+dst = cv.merge((cv.medianBlur(blue, ksize=3), cv.medianBlur(green, ksize=3), cv.medianBlur(red, ksize=3)))
 
 # 処理時間の計測終了
 end_time = time.time()
@@ -21,6 +21,6 @@ end_time = time.time()
 print("Processing Time: {:.6f} seconds".format(end_time - start_time))
 
 # 結果を出力
-cv.imwrite("C:/github/sample/python/opencv/filter2d/integral-image/output.jpg", dst)
+cv.imwrite("C:/github/sample/python/opencv/filter2d/integral-image/output4.jpg", dst)
 
-# Processing Time: 0.013000 seconds
+# Processing Time: 0.002890 seconds
