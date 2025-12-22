02b9c4b18d60aebc17dadac34c6cd31a6d9aaabc
Nishizumi <34793405+nishizumi-lab@users.noreply.github.com>
2025-12-23 01:08:26 +0900

    add

--- python/numpy/fast-tutorial/sample07.py
+++ python/numpy/fast-tutorial/sample07.py
@@ -13,3 +13,10 @@ print(selected)  # [100 105]
 # True の要素のみ値を代入（例：特定日の株価を補正）
 prices[mask] = 999
 print(prices)  # [999 102  98 999 110]
+
+# マスク用の配列を生成（900以上だけ抽出）
+print(prices >= 900) # [ True False False  True False]
+
+# 900以上の要素を0にする
+prices[prices >= 900] = 0
+print(prices)  # [  0 102  98   0 110]
